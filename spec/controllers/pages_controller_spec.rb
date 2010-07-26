13436a73c22023e80879ca0e8825c05322a85e19
Urzuae <urzuae@gmail.com>
2010-07-26 17:47:49 -0500

    Refactor rspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   #Delete these examples and add some real ones
   it "should use PagesController" do
     controller.should be_an_instance_of(PagesController)
@@ -18,7 +22,7 @@ describe PagesController do
 
   it "should have the right title" do
     get 'home'
-    response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Home")
+    response.should have_tag("title", @base_title + " | Home")
   end
 
   describe "GET 'contact'" do
@@ -30,7 +34,7 @@ describe PagesController do
 
   it "should have the right title" do
     get 'contact'
-    response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Contact")
+    response.should have_tag("title", @base_title + " | Contact")
   end
   
   describe "GET 'about'" do
@@ -42,7 +46,7 @@ describe PagesController do
 
   it "should have the right title" do
     get 'about'
-    response.should have_tag("title", "Ruby on Rails Tutorial Sample App | About")
+    response.should have_tag("title", @base_title + " | About")
   end
   
   describe "GET 'help'" do
@@ -54,7 +58,7 @@ describe PagesController do
   
   it "should have the right title" do
     get 'help'
-    response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Help")
+    response.should have_tag("title", @base_title + " | Help")
   end
   
 end
