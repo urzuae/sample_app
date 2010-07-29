34a447071708e2cffda5f466b4f013c8904eecf5
Urzuae <urzuae@gmail.com>
2010-07-28 21:10:45 -0500

    Set up users show page and user model with password

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe UsersController do
   integrate_views
 
+
   #Delete these examples and add some real ones
   it "should use UsersController" do
     controller.should be_an_instance_of(UsersController)
@@ -18,5 +19,34 @@ describe UsersController do
   it "should have the right title" do
     get 'new'
     response.should have_tag("title", /Sign up/)
+  end
+  
+  describe "get 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+      User.stub!(:find, @user.id).and_return(@user)
+    end
+    
+    it "should be succesful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_tag("title", /#{@user.name}/)
+    end
+    
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_tag("h2", /#{@user.name}/)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_tag("h2>img", :class => "gravatar")
+    end
+    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @attributes = { :name => "user", :email => "user@name.com" }
+    @attributes = { 
+      :name => "user",
+      :email => "user@name.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given valid attributes" do
@@ -45,5 +50,72 @@ describe User do
     User.create!(@attributes)
     user_duplicate = User.new(@attributes)
     user_duplicate.should_not be_valid
+  end
+  
+  describe "password validations" do
+    
+    it "should require a password" do
+      User.new(@attributes.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attributes.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attributes.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attributes.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+  
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attributes)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    describe "has_password? method" do
+      
+      it "should be true if the password match" do
+        @user.has_password?(@attributes[:password]).should be_true
+      end
+      
+      it "should be false if the password do not match" do
+        @user.has_password?("failed").should be_false
+      end
+      
+      describe "authenticate method" do
+        
+        it "should return nil on email/password mismatch" do
+          user_wrong = User.authenticate(@attributes[:email], "wrongpass")
+          user_wrong.should be_nil
+        end
+        
+        it "should return nil for an email address with no user" do
+          user_none = User.authenticate("bar@foo.com", @attributes[:password])
+          user_none.should be_nil
+        end
+        
+        it "should return the user on email/passwors match" do
+          user_match = User.authenticate(@attributes[:email], @attributes[:password])
+          user_match.should == @user
+        end
+        
+      end
+    end
   end
 end
